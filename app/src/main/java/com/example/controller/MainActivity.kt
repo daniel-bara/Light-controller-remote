ef38a8399d912da430876aa19bb7105b2b7ab742
xix22 <b.danib@hotmail.com>
2019-11-11 15:41:31 +0100

    code cleanup

--- app/src/main/java/com/example/controller/MainActivity.kt
+++ app/src/main/java/com/example/controller/MainActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
-import android.Manifest
 import android.view.MenuItem
 import android.view.View
-import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
@@ -26,40 +24,45 @@ import android.content.SharedPreferences
 import android.view.inputmethod.InputMethodManager
 import androidx.core.app.ActivityCompat
 import com.google.android.material.snackbar.Snackbar
-import java.sql.Timestamp
 
 
 class MainActivity : AppCompatActivity() {
-    var url = ""
-    val colorDelayMillis:Long = 500
-    lateinit var textvw:TextView
-    lateinit var edittxt:EditText
-    private lateinit var queue:RequestQueue
-    var sendingColorQueue= 0
-    lateinit var argbEnvelope: ColorEnvelope
-    val continuousSender = Handler()
+//region constants
+    private val alarmEarlierBySeconds:Int =60
+    private val colorDelayMillis:Long = 500
+    //endregion
+
+// region variables
+    private lateinit var textvw:TextView
+    private lateinit var edittxt:EditText
     private lateinit var sharedPreferences:SharedPreferences
 
+    var url = ""
+    private lateinit var queue:RequestQueue
+    var sendingColorQueue = 0
+    lateinit var argbEnvelope: ColorEnvelope
+    val continuousSender = Handler()
+    // endregion
 
-
-
-
+    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         sharedPreferences = getPreferences(MODE_PRIVATE)
-        ActivityCompat.requestPermissions(this, Array(1){"com.urbandroid.sleep.READ"}, 2)
-        url = sharedPreferences.getString("url","http://192.168.2.104:8766")
         edittxt = findViewById(R.id.editText)
         textvw= findViewById(R.id.tvw)
-        fab.setOnClickListener {
+
+        MailButton.setOnClickListener {
             sendRaw(edittxt.text.toString())
-            //Log.d("TAG", "l;ask")
         }
         queue = Volley.newRequestQueue(this)
+        ActivityCompat.requestPermissions(this, Array(1){"com.urbandroid.sleep.READ"}, 2)
+        url  =  sharedPreferences.getString("url","http://192.168.2.105:8766")
         updateTextView()
 
+        //region ColorPicker, Slider
         val delayOver = object:Runnable { override fun run() {
+            //this runs every time the countdown is over
             if(sendingColorQueue == 1) {
                 sendColor()
             }
@@ -68,14 +71,14 @@ class MainActivity : AppCompatActivity() {
                 continuousSender.postDelayed(this, colorDelayMillis)
             }
             Log.i("sending", sendingColorQueue.toString())
-
         } }
         continuousSender.postDelayed(delayOver, 1000)
         colorPickerView.setColorListener(object : ColorEnvelopeListener {
             override fun onColorSelected(envelope: ColorEnvelope, fromUser: Boolean) {
                 argbEnvelope = envelope
-                if(sendingColorQueue ==0){continuousSender.postDelayed(delayOver, colorDelayMillis)}
                 if(sendingColorQueue<2){
+                    if(sendingColorQueue ==0)
+                        {continuousSender.postDelayed(delayOver, colorDelayMillis)}
                     sendingColorQueue=2
                     sendColor()
                 }
@@ -85,8 +88,10 @@ class MainActivity : AppCompatActivity() {
         colorPickerView.preferenceName= "MyColorPicker"
         val alphaSlideBar: AlphaSlideBar = findViewById(R.id.alphaSlideBar)
         colorPickerView.attachAlphaSlider(alphaSlideBar)
+        //endregion
 
         edittxt.setOnKeyListener(object : View.OnKeyListener {
+            // close virtual keyboard on enter
             override fun onKey(v: View, keyCode: Int, event: KeyEvent): Boolean {
                 // If the event is a key-down event on the "enter" button
                 if (event.action == KeyEvent.ACTION_DOWN && keyCode == KEYCODE_ENTER) {
@@ -116,7 +121,7 @@ class MainActivity : AppCompatActivity() {
         send(argbSelected)
     }
     private fun updateTextView(){
-        textvw.text = "Sending commands to $url"
+        textvw.text = getString(R.string.destination, url)
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -133,7 +138,8 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
-    fun onClickTurnOn(v: View){
+
+    fun onClickTurnOn(@Suppress("UNUSED_PARAMETER")v: View){
         val params = HashMap<String, String>()
         params["lightOn"] = "1"
         send(params)
@@ -145,7 +151,7 @@ class MainActivity : AppCompatActivity() {
 
         Snackbar.make(v, "Alarm set " + nextAlarmSeconds()/3600 + " hours " + nextAlarmSeconds()%3600/60 + " minutes from now", Snackbar.LENGTH_LONG)
             .setAction("Action", null).setDuration(5000).show()
-        params["alarmIn"] = (nextAlarmSeconds()-60).toString()
+        params["alarmIn"] = (nextAlarmSeconds()-alarmEarlierBySeconds).toString()
         params["lightOn"] = "0"
         send(params)
     }
@@ -187,12 +193,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun sendRaw(text:String){
-        val postRequest = object: StringRequest(Request.Method.GET, "$url/?$text", Response.Listener {}, Response.ErrorListener{}){}
+        val postRequest = object: StringRequest(Method.GET, "$url/?$text", Response.Listener {}, Response.ErrorListener{}){}
         queue.add(postRequest)
         Log.d("TAG", "Contacting $url/?$text")
     }
     private fun send(parameters:HashMap<String, String>){
-        val postRequest = object: StringRequest(Request.Method.POST, url, Response.Listener {}, Response.ErrorListener{}) {
+        val postRequest = object: StringRequest(Method.POST, url, Response.Listener {}, Response.ErrorListener{}) {
             override fun getBodyContentType():String {
                 return "application/x-www-form-urlencoded"
             }
